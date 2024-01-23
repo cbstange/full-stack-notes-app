e54503819e206c2c66d87a3e359bd680c716086d
cbstange <>
2024-01-23 19:13:23 +0000

    create base template

---
+++ website/auth.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask_login import login_user, login_required, logout_user, current_user
+
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+
+@auth.route('/sign-up')
+def sign_up():
+    return "<p>Sign Up</p>"
